1d4ce767e33f38609b0476d196ddf1545d212918
Joshua Buchacher <jbuchacher@ubermind.com>
2015-03-11 14:34:00 -0700

    add more instruments and analog readings

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ var socket = io
     .of('/soundsocket') // establish a route to connect to using the Socket.IO client
     .on('connection', function (socket) {
       console.log('client connected') // each time a client establishes a connection, log a message to the console that the app is running on (on the Edison).
-      socket.emit('test_message', 'some data')
     })
 
 // this will be called when Cylon is fully initialized, which is when we should open up our WebSocket connection.
@@ -27,22 +26,73 @@ var cylonReady = function(my) {
 
 // this will be called each time a socket is opened, so each client will receive their own events when buttons are pushed.
 var registerSocketHandlers = function(my, socket) {
-  my.button.on('push', function() {
-    socket.emit('button', 'push')
+  buttons = [my.button0, my.button1, my.button2, my.button3]
+  for (var i = 0; i < buttons.length; i++) {
+    var button = buttons[i];
+    registerButtonHandler(socket, button, i);
+  }
+
+  analogSensors = [my.potentiometer, my.photocell]
+  for (var i = 0; i < analogSensors.length; i++) {
+    var sensor = analogSensors[i];
+    registerAnalogSensorHandler(socket, sensor, i);
+  }
+}
+
+// convenience for button specific events
+var registerButtonHandler = function(socket, button, buttonID) {
+  // on push, send button ID and value of 1
+  button.on('push', function() {
+    socket.emit('button', {
+      'id': buttonID,
+      'value': 1
+    })
   })
 
-  my.button.on('release', function() {
-    socket.emit('button', 'release')
+  // on push, send button ID and value of 0
+  button.on('release', function() {
+    socket.emit('button', {
+      'id': buttonID,
+      'value': 0
+    })
   })
 }
 
-cylon.robot({
-  connections: { // tell Cylon how we will be connecting to our devices
-    edison: { adaptor: 'intel-iot' }
-  },
-  devices: {
-    button: { driver: 'button', pin: 2 }
+// convenience for listening to analog read events and emitting sensor data messages
+var registerAnalogSensorHandler = function(socket, analogSensor, analogSensorID) {
+  // on new data, send sensor ID and value of sensor reading
+  analogSensor.on('analogRead', function() {
+    sensorValue = analogSensor.analogRead()
+
+    socket.emit('sensor', {
+      'id': analogSensorID,
+      'value': sensorValue
+    })
+  })
+}
+
+// tell Cylon which devices we will be interfacing with
+var getDevices = function() {
+  return {
+    button0: { driver: 'button', pin: 2 },
+    button1: { driver: 'button', pin: 3 },
+    button2: { driver: 'button', pin: 4 },
+    button3: { driver: 'button', pin: 5 },
+    potentiometer: { driver: 'analogSensor', pin: 0, lowerLimit: 100, upperLimit: 900 },
+    photocell: { driver: 'analogSensor', pin: 1, lowerLimit: 100, upperLimit: 900 }
   }
+}
+
+// tell Cylon how we will be connecting to our devices
+var getConnections = function() {
+  return {
+    edison: { adaptor: 'intel-iot' }
+  }
+}
+
+cylon.robot({
+  connections: getConnections(),
+  devices: getDevices()
 }).on('ready', cylonReady)
 
 cylon.start()
--- public/js/playsounds.js
+++ public/js/playsounds.js
@@ -1,8 +1,68 @@
-var BD;
+var BD
+var SD
+var HH1
+var HH2
+var CYM
+
+var drum
+var lead
+var env
+var arp
+var delay
+var inv
 
 T("audio").load("./drumkit.wav", function() {
-  BD = this.slice(0,  500).set({bang:false})
-  drum = T("lowshelf", {freq:110, gain:8, mul:0.6}, BD).play()
+  BD  = this.slice(   0,  500).set({bang:false})
+  SD  = this.slice( 500, 1000).set({bang:false})
+  HH1 = this.slice(1000, 1500).set({bang:false, mul:0.2})
+  HH2 = this.slice(1500, 2000).set({bang:false, mul:0.2})
+  CYM = this.slice(2000).set({bang:false, mul:0.2})
+
+  var scale = new sc.Scale([0,1,3,7,8], 12, "Pelog")
+
+  var P1 = [
+    [BD, HH1],
+    [HH1],
+    [HH2],
+    [],
+    [BD, SD, HH1],
+    [HH1],
+    [HH2],
+    [SD],
+  ].wrapExtend(128)
+
+  var P2 = sc.series(16)
+
+  drum = T("lowshelf", {freq:110, gain:8, mul:0.6}, BD, SD, HH1, HH2, CYM).play()
+  lead = T("saw", {freq:T("param")})
+  env  = T("perc", {r:100})
+  arp  = T("OscGen", {wave:"sin(15)", env:env, mul:0.5})
+
+  delay = T("delay", {time:"BPM128 L4", fb:0.65, mix:0.35},
+            T("pan", {pos:T("tri", {freq:"BPM64 L1", mul:0.8}).kr()}, arp)
+           ).play();
+
+  inv = T("interval", {interval:"BPM128 L16"}, function(count) {
+    var i = count % P1.length
+
+    if (i === 0) CYM.bang()
+
+    P1[i].forEach(function(p) {
+      p.bang()
+    })
+
+    if (Math.random() < 0.015) {
+      var j = (Math.random() * P1.length)|0;
+      P1.wrapSwap(i, j);
+      P2.wrapSwap(i, j);
+    }
+
+    var noteNum = scale.wrapAt(P2.wrapAt(count)) + 60;
+    if (i % 2 === 0) {
+      lead.freq.linTo(noteNum.midicps() * 2, "100ms");
+    }
+    arp.noteOn(noteNum + 24, 60)
+  }).start()
 })
 
 var socketConnection = io.connect('http://192.168.1.11:8080/soundsocket')
@@ -11,8 +71,42 @@ socketConnection.on('connect', function () {
   console.log('connected to socket')
 })
 
-socketConnection.on('button', function (value) {
-  if (value == 'push') {
-    BD.bang()
+socketConnection.on('button', function (data) {
+  buttonID = data['id']
+  value = data['value']
+
+  switch (buttonID) {
+  case 0:
+    if (value == 1) {
+      BD.bang()
+    }
+    break;
+  case 1:
+    if (value == 1) {
+      CYM.bang()
+    }
+    break;
+  case 2:
+    value == 1 ? HH1.set({mul: 1}) : HH1.set({mul: 0.2})
+    break;
+  case 3:
+    value == 1 ? HH2.set({mul: 1}) : HH2.set({mul: 0.2})
+    break;
+  default:
+  }
+})
+
+socketConnection.on('sensor', function (data) {
+  sensorID = data['id']
+  value = data['value']
+
+  switch (sensorID) {
+  case 0:
+    inv.set({interval: value * 2 })
+    break;
+  case 1:
+    arp.set({mul: 10 / value })
+    break;
+  default:
   }
 })
