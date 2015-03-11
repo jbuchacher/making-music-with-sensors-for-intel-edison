9a8520d9487a6f68ca58b2293b2484db565d980e
Joshua Buchacher <jbuchacher@ubermind.com>
2015-03-10 20:05:33 -0700

    added cylon and bootstrap it on initialization

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express') 
 var app = express() // initialize express
 var server = require('http').Server(app) // give http module the express server
+var cylon = require('cylon')
+
 var io = require('socket.io')(server) // pass our http server to Socket.IO
 
 app.use(express.static(__dirname + '/public')) // tell express to serve anything inside of the public directory
@@ -13,3 +15,16 @@ var socket = io
     console.log('client connected') // each time a client establishes a connection, log a message to the console that the app is running on (on the Edison).
     socket.emit('test_message', 'some data')
   })
+
+cylon.robot({
+  connections: { // tell Cylon how we will be connecting to our devices
+    edison: { adaptor: 'intel-iot' }
+  },
+  devices: {
+    // we don't have devices to add yet, but we will shortly!
+  }
+}).on('ready', function(my) {
+  console.log('cylon ready')
+})
+
+cylon.start()
