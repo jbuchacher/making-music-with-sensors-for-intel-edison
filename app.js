dd787ed5c66227de182d6d7a0bc8b25f635951f1
Joshua Buchacher <jbuchacher@ubermind.com>
2015-03-10 20:08:59 -0700

    added single button to cylon config, and log message to console on push and release

--- app.js
+++ app.js
@@ -10,21 +10,29 @@ app.use(express.static(__dirname + '/public')) // tell express to serve anything
 server.listen(8080) // tell express to start listening for requests on port 8080
 
 var socket = io
-  .of('/soundsocket') // establish a route to connect to using the Socket.IO client
-  .on('connection', function (socket) {
-    console.log('client connected') // each time a client establishes a connection, log a message to the console that the app is running on (on the Edison).
-    socket.emit('test_message', 'some data')
-  })
+    .of('/soundsocket') // establish a route to connect to using the Socket.IO client
+    .on('connection', function (socket) {
+      console.log('client connected') // each time a client establishes a connection, log a message to the console that the app is running on (on the Edison).
+      socket.emit('test_message', 'some data')
+    })
 
 cylon.robot({
   connections: { // tell Cylon how we will be connecting to our devices
     edison: { adaptor: 'intel-iot' }
   },
   devices: {
-    // we don't have devices to add yet, but we will shortly!
+    button: { driver: 'button', pin: 2 }
   }
 }).on('ready', function(my) {
   console.log('cylon ready')
+
+  my.button.on('push', function() { // when the button is pushed, this callback will be triggered
+    console.log("Button pushed!")
+  })
+
+  my.button.on('release', function() { // when the button is released, this callback will be triggered
+    console.log("Button released!")
+  })
 })
 
 cylon.start()
